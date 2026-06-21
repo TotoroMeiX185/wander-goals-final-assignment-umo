2096996fca592a361398e710dadfe862093fcc35
Sachini Liyanage <148330362+TotoroMeiX185@users.noreply.github.com>
2026-06-21 14:44:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+/*splashscreen*/
+
+// Redirect after 5 seconds
+setTimeout(() => {
+    window.location.href = "Homepage.html";
+}, 5000);
+
+/*homescreen*/
+function goTo(Page) {
+    window.location.href = Page;
+}
+
